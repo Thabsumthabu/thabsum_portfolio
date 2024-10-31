38ea4531fa98606555609a1c61c61e04e7d2dc94
Thabsumthabu <thabsumjamal123@gmail.com>
2024-10-31 21:10:28 +0530

    inital commit

---
+++ README.md
@@ -0,0 +1,78 @@
+## Shyam_portfolio
+## Personal Portfolio Website
+
+This is a personal portfolio website showcasing the work and skills of a front-end developer and student. The portfolio is built using HTML, SCSS, JavaScript, and Bootstrap, providing an interactive and visually appealing user experience.
+
+## Technologies Used
+
+The following technologies were used to build this portfolio website:
+
+1. **HTML:** Used for the website's structure and content.
+
+2. **SCSS:** A CSS preprocessor that enhances the styling workflow by providing features like variables, nesting, and mixins.
+
+3. **JavaScript:** Used for interactivity and dynamic behavior on the website.
+
+4. **Bootstrap:** A popular front-end framework that provides a collection of CSS and JavaScript components for building responsive and mobile-first websites.
+
+## Features
+
+The portfolio website includes the following features:
+
+1. **Responsive Design:** The website is built with responsiveness in mind, ensuring optimal viewing experience across different devices and screen sizes.
+
+2. **Project Showcase:** A section dedicated to showcasing my projects, including descriptions and links to live demos or repositories.
+
+3. **Skills and Expertise:** Highlights my key skills and areas of expertise, providing visitors with an overview of my technical abilities.
+
+4. **About Me:** A brief introduction to who I am, my background, and my passion for what I do.
+
+5. **Resume/CV:** A downloadable version of my resume or CV, allowing visitors to easily access and review my qualifications.
+
+6. **Contact :** Links in footer that enables visitors to reach out to me directly, sending messages and inquiries.
+
+## Setup
+
+To set up the project locally, follow these steps:
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/Shyam_portfolio.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd Shyam_portfolio
+   ```
+
+3. Customize the website according to your preferences. Update the HTML, SCSS, and JavaScript files to reflect your own information, projects, and design choices.
+
+4. Open the `index.html` file in your preferred web browser to view the website.
+
+## Deployment
+
+To deploy the portfolio website to a hosting platform or web server, follow these steps:
+
+1. Build the project by compiling the SCSS files and bundling the JavaScript:
+
+   ```bash
+   sass styles.scss styles.css  # Compile SCSS to CSS
+   ```
+
+   You may need to use a SCSS compiler of your choice or configure a build process depending on your setup.
+
+2. Upload the HTML, CSS, JavaScript, and any other relevant files to your hosting platform or web server.
+
+3. Ensure that your hosting platform supports the required technologies (HTML, CSS, JavaScript) and that they are correctly configured.
+
+4. Access your deployed portfolio website using the provided URL or the domain associated with your hosting platform.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvement, please feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE). Feel free to modify and customize the code to suit your needs.
---
+++ index.js
@@ -0,0 +1,132 @@
+// -------------------------dark mode functionality----------------------------------------------------
+
+const darkmodeFun = () => {
+  var mode;
+
+  // ----------toggle and make it dark mode-------------------
+  document.querySelector(".darkmode").classList.toggle("lightmode");
+
+  let navlinks = document.querySelectorAll(".navlinks");
+  for (let i = 0; i < navlinks.length; i++) {
+    navlinks[i].classList.toggle("lightmode");
+  }
+
+  // ----------check if is in darkmode class is present--------------------------
+  // ----------if no add class darkmode to document ++++ if yes then removes-------
+  if (document.querySelector(".darkmode").classList.contains("lightmode")) {
+    // alert("light")
+    mode = "light";
+  } else {
+    // alert("dark")
+    mode = "dark";
+  }
+  //-------------set the local storage by json.stringify to convert it into string--------------------
+  // ------------it send to local storage as object, hence use this to convert to strings-----------
+  localStorage.setItem("pageMode", JSON.stringify(mode));
+};
+
+// ------------------after the function runs either it will have class lightmode or darkmode--------------
+// ------------------get the key from local storage and set it tp getmode then if dark then set the whole body to darkmode------------
+let getMode = JSON.parse(localStorage.getItem("pageMode"));
+// alert(getMode)
+
+if (getMode == "light") {
+  document.body.classList.add("lightmode"); //important line to set darkmode
+}
+
+// ----------------------------intersection observer animation--------------------------------------
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const animation = document.querySelectorAll(".hidden");
+animation.forEach((div) => observer.observe(div));
+//passing each of the div with hidden to customObserver.observe()
+
+// ----------------------------download link for resume + links(social media)--------------------------------------
+// https://drive.google.com/file/d/1-uP_WQPEHEijsOPtSJ4-WmYsHgyIuG8H/view?usp=share_link
+const downloadFun = () => {
+  window.open(
+    "https://drive.google.com/file/d/1RrE0Rggxh_GRls8C0d7t7fuqI0v54UAV/view?usp=sharing"
+  );
+};
+
+const github = () => {
+  window.open("https://github.com/Thabsumthabu", "_blank");
+};
+const linkedin = () => {
+  window.open("https://www.linkedin.com/in/thabsum-pj-b15107335/", "_blank");
+};
+const twitter = () => {
+  window.open("https://x.com/ThabsumPj", "_blank");
+};
+const instagram = () => {
+  window.open("https://www.instagram.com/shyam_g__/", "_blank");
+};
+
+const project1 = () => {
+  window.open("https://thabsumthabu.github.io/netflixclone//", "_blank");
+};
+const github1 = () => {
+  window.open("https://github.com/Thabsumthabu/netflixclone", "_blank");
+};
+
+const project2 = () => {
+  window.open("https://thabsumthabu.github.io/starbucksnewclone/", "_blank");
+};
+const github2 = () => {
+  window.open("https://github.com/Thabsumthabu/starbucksnewclone/tree/master", "_blank");
+};
+// const project3 = () => {
+//   window.open("https://todo-by-shyam.netlify.app/", "_blank");
+// };
+// const github3 = () => {
+//   window.open("https://github.com/shyamg090/todo-react/", "_blank");
+// };
+
+// ---------------pop up ---------------------------------------------------
+const popupDiv = document.querySelector(".customPopup");
+// const popup = document.querySelector(".popup");
+
+window.onload = () => {
+  // console.log("helloloaded");
+  popup.style.display = "block";
+};
+
+const popupClicked = () => {
+  popupDiv.style.display = "none";
+};
+
+
+const quote = fetch('https://type.fit/api/quotes').then( (res) => res.json() ).then( (data) => {
+  console.log(data);
+  let random = 0; 
+  random = Math.floor((Math.random() * 15) + 1);
+
+  const quoteLine = document.querySelector('.quote-line');
+  quoteLine.innerHTML = data[random]?.text;
+
+  const author = document.querySelector('.author');
+  const authorname = data[random]?.author;
+  const n = authorname.length;
+
+  n>11 ? author.innerHTML = authorname.slice(0, n - 10) : author.innerHTML = "Yogi Beera"
+  // console.log(authorname.slice(0, n - 10));
+  // author.innerHTML = authorname.slice(0, n - 10);
+})
+
+useEffect(() => {
+  random();
+  quote();
+
+  return () => {
+    console.log('error');
+  }
+}, [])
